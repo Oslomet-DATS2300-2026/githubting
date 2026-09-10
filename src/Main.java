69ccd6605c5b741dfaf3fb8d6a07b274f344721f
Nikolai Bjørnestøl Hansen <nikobjo@oslomet.no>
2026-09-10 14:45:57 +0200

    La til en planet.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hi jupiter!");
         System.out.println("Good day venus!");
         System.out.println("You are not a planet pluto.");
+        System.out.println("You are a planet, mars.");
     }
 }
