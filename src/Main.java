9970f8c90c3dce56a4ca74ab63d122b4d4c9698a
Nikolai Bjørnestøl Hansen <nikobjo@oslomet.no>
2026-09-10 14:56:31 +0200

    Lagde en veldig viktig funksjon.

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,8 @@ public class Main {
         System.out.println("You are not a planet pluto.");
         System.out.println("You are also not a planet, Mickey.");
     }
+
+    public static int girUtFem() {
+        return 5;
+    }
 }
