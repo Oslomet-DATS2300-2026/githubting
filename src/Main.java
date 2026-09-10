bf2a62a81e05603bc5af10f9e8e3f95b5df6689c
Nikolai Bjørnestøl Hansen <nikobjo@oslomet.no>
2026-09-10 14:42:12 +0200

    La til koselige meldinger (hater pluto)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi jupiter!");
+        System.out.println("Good day venus!");
+        System.out.println("You are not a planet pluto.");
     }
 }
