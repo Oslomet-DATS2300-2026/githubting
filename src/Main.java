dc4c7ec18bcfbed07ab0ea39e1622f95ced80357
Nikolai Bjørnestøl Hansen <nikobjo@oslomet.no>
2026-09-10 14:44:47 +0200

    Nå handler det om dyr.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi jupiter!");
-        System.out.println("Good day venus!");
+        System.out.println("Hi mollusc!");
+        System.out.println("Good day donkey!");
         System.out.println("You are not a planet pluto.");
     }
 }
